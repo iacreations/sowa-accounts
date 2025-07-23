ff1d5e96689c22f8bc4597990776018b9b2ad30e
iacreations <iacreations256@gmail.com>
2025-07-23 08:52:30 +0000

    signup changes

--- sowafinance/sowaAuth/views.py
+++ sowafinance/sowaAuth/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.conf import settings
+from django.core.mail import EmailMessage
+from django.utils import timezone
+from django.urls import reverse
 # Create your views here.
-def login_user(request):
-    return render(request, 'authenticate/loin_signup.html')
+def register_user(request):
+
+    return render(request, 'authenticate/login_signup.html')
